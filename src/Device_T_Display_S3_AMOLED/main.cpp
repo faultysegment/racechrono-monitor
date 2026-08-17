523a227b37e11480fdc4bf25e9763fb63dd6c48e
Ilya Skrypitsa <ilya.skrypitsa@gmail.com>
2026-08-17 23:06:34 +0300

    refactor(amoled): clean up main.cpp entry point and remove obsolete global pointers

--- src/Device_T_Display_S3_AMOLED/main.cpp
+++ src/Device_T_Display_S3_AMOLED/main.cpp
@@ -12,10 +12,6 @@
 #include "../../App.h"
 #include "Policies/AmoledViewPolicy.h"
 
-Arduino_DataBus *bus = NULL;
-Arduino_GFX *gfx = NULL;
-Arduino_Canvas *canvas = NULL;
-
 App<AmoledDisplayPolicy, AmoledHWPolicy, AmoledBLEPolicy, AmoledStoragePolicy, AmoledViewPolicy<AmoledDisplayPolicy>> app;
 
 void uiTask(void* pvParameters) {
@@ -46,17 +42,16 @@ void logicTask(void* pvParameters) {
 void setup() {
     app.setup();
 
-    // Pin Input to Core 0 (PRO_CPU) for dedicated high-priority I2C touch polling
+    // Pin Input to Core 0 (PRO_CPU) for dedicated high-priority touch polling
     xTaskCreatePinnedToCore(inputTask, "Input_Task", 4096, NULL, 5, NULL, 0);
 
     // Pin UI Rendering to Core 1 (APP_CPU) for dedicated display rendering
     xTaskCreatePinnedToCore(uiTask, "UI_Task", 4096, NULL, 1, NULL, 1);
-    
+
     // Pin Logic to Core 0 (PRO_CPU)
     xTaskCreatePinnedToCore(logicTask, "Logic_Task", 4096, NULL, 1, NULL, 0);
 }
 
 void loop() {
-    // The main loop task can just be suspended, we do all work in our specific tasks
     vTaskSuspend(NULL);
 }
